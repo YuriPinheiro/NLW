573d667cecbe4e02b70264651d5a2a1eed2aff22
Yuri Pinheiro <ypbernardi@ucs.br>
2025-07-09 19:22:12 -0300

    Feat: RoomsList, RoomForm, RoomDetail

--- web/src/pages/create-room.tsx
+++ web/src/pages/create-room.tsx
@@ -1,46 +1,18 @@
-import { Button } from "@/components/ui/button";
-import { useQuery } from "@tanstack/react-query";
-import { Link } from "react-router-dom";
-
-type GetRoomsAPIResponse = Array<{
-    id: string
-    name: string
-}>
+import { CreateRoomForm } from "@/components/create-room-form";
+import { RoomList } from "@/components/rooms-list";
 
 export function CreateRoom() {
 
-    const { data, isLoading } = useQuery({
-        queryKey: ['get-rooms'],
-        queryFn: async () => {
-            const response = await fetch('http://localhost:3333/rooms')
-            const result: GetRoomsAPIResponse = await response.json();
 
-            return result;
-        }
-    })
 
     return (
-        <div>
-            <div>Create Room</div>
-            {isLoading &&
-                <div> Carregando... </div>
-            }
-            <div className="flex flex-col gap-1">
-                {data && data.map((room) => {
-                    return (
-                        <div key={room.id}>
-                            <p>{`Nome: ${room.name}`}</p>
-                            <Button variant="link">
-                                <Link to={`/room/${room.id}`}>
-                                    Acessar
-                                </Link>
-                            </Button>
-                        </div>
-                    )
-                })}
+        <div className="min-h-screen px-4 py-8">
+            <div className="mx-auto max-w-4xl">
+                <div className="grid grid-cols-2 items-start gap-8">
+                    <CreateRoomForm />
+                    <RoomList />
+                </div>
             </div>
-
-            <Link className="underline" to={"/room"}>Acessar</Link>
         </div>
     )
 }
